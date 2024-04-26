12cd355ed53ac180f15ed33ea529b766de66d5d4
mehedi-hasan02 <mehedisani02@gmail.com>
2024-04-26 20:23:04 +0600

    implement spot-details

--- src/Components/SpotDetails/SpotDetails.jsx
+++ src/Components/SpotDetails/SpotDetails.jsx
@@ -1,14 +1,28 @@
 import { Link, useLoaderData } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
+import { IoMdTime } from "react-icons/io";
 
 const SpotDetails = () => {
     const loadedSpot = useLoaderData();
-    const { _id, image, country, spotName, location, season, avgCost, TravelTime, TotalVisitor, } = loadedSpot;
+    const { image, country, spotName, location, season, avgCost, TravelTime, TotalVisitor,shortDescription } = loadedSpot;
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">New movie is released!</h2>
-                <p>Click the button to watch on Jetflix app.</p>
+            <figure><img src={image} alt="Movie"/></figure>
+            <div className="p-5 space-y-2">
+                <h2 className="card-title">{spotName}</h2>
+                <p><span>Description: </span>{shortDescription}</p>
+                <div className="flex gap-2">
+                    <p className="flex items-center"><CiLocationOn />{location},</p>
+                    <p>{country}</p>
+                </div>
+                <div className="flex gap-40">
+                    <p className="flex items-center"><IoMdTime />{TravelTime}</p>
+                    <p>Avg Cost: ${avgCost}</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p>Best Season: {season}</p>
+                    <p>Total Visitor: {TotalVisitor}</p>
+                </div>
                 <div className="card-actions">
                     <Link to='/'>
                         <button className="btn bg-green-500 text-white hover:bg-green-500">Go To Home</button>
